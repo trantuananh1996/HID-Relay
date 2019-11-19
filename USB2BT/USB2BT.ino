50a422a611814e82bb033878290f6409db96b48d
Trần Anh <trantuananh1996@gmail.com>
2019-11-19 22:39:07 +0700

    First runable with HIDMultiMediaKbd

--- USB2BT/USB2BT.ino
+++ USB2BT/USB2BT.ino
@@ -1,18 +1,19 @@
 #include <hidboot.h>
 #include <usbhid.h>
 #include <usbhub.h>
+#include <hidcomposite.h>
 
 // #define _DEBUG
-#define _COMBO
+#define _COMBO = 0
 
 #ifdef _DEBUG
-   #define DEBUG_PRINT(str) Serial.print (str)
-   #define DEBUG_PRINTF(str, format) Serial.print (str, format)
-   #define DEBUG_PRINTLN(str)  Serial.println (str)
+#define DEBUG_PRINT(str) Serial.print(str)
+#define DEBUG_PRINTF(str, format) Serial.print(str, format)
+#define DEBUG_PRINTLN(str) Serial.println(str)
 #else
-   #define DEBUG_PRINT(x)
-   #define DEBUG_PRINTF(x, format)
-   #define DEBUG_PRINTLN(x)
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTF(x, format)
+#define DEBUG_PRINTLN(x)
 #endif
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
@@ -20,119 +21,129 @@
 #include <spi4teensy3.h>
 #include <SPI.h>
 #endif
+#include <avr/power.h>
 
-class HIDRelay : public HIDReportParser 
-{
-    protected:
-    virtual void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+class HIDRelay : public HIDComposite {
+ public:
+  HIDRelay(USB *p) : HIDComposite(p){};
+
+ protected:
+  virtual void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id,
+                            uint8_t len, uint8_t *buf);
+  bool SelectInterface(uint8_t iface, uint8_t proto);
 };
 
-void HIDRelay::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
-//    DEBUG_PRINT("Relay ");
-//    DEBUG_PRINTLN(len);
+// Return true for the interface we want to hook into
+bool HIDRelay::SelectInterface(uint8_t iface, uint8_t proto) {
+  if (proto != 0) return true;
 
-    for (uint8_t i = 0; i < len; i++) {
-            Serial.write((uint8_t)buf[i]);
+  return false;
+}
 
-//            DEBUG_PRINTF((uint8_t)buf[i], HEX);
-//            DEBUG_PRINT(",");
-    }
-    DEBUG_PRINT("\r\n");
+void HIDRelay::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id,
+                            uint8_t len, uint8_t *buf) {
+  //    DEBUG_PRINT("Relay ");
+  //    DEBUG_PRINTLN(len);
+
+  for (uint8_t i = 0; i < len; i++) {
+    Serial.write((uint8_t)buf[i]);
+
+    //            DEBUG_PRINTF((uint8_t)buf[i], HEX);
+    //            DEBUG_PRINT(",");
+  }
+  DEBUG_PRINT("\r\n");
 };
 
-class KbdRptParser : public HIDRelay 
-{
-    void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+class KbdRptParser : public HIDRelay {
+ public:
+  KbdRptParser(USB *p) : HIDRelay(p){};
+  void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len,
+                    uint8_t *buf);
 };
 
-void KbdRptParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
-//    DEBUG_PRINTLN("Kbd");
+void KbdRptParser::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id,
+                                uint8_t len, uint8_t *buf) {
+  //    DEBUG_PRINTLN("Kbd");
 
-    Serial.write((uint8_t)0xFD); // BT-HID: Start Byte
-    // Serial.write((uint8_t)0x09); // BT-HID: Data Length
-    Serial.write((uint8_t)len+1); // BT-HID: Data Length
-    Serial.write((uint8_t)0x01); // BT-HID: Descriptor (1=Keyboard, 2=Mouse, 3=KybdMouse combo)
-    // Keyboard Data consists of a single byte for modifiers (Ctrl, alt, etc)
-    // Followed by 0x00
-    // Followed by 6 key codes (any or all may be 0x00 for no key pressed)
-    HIDRelay::Parse(hid,is_rpt_id,len,buf);
+  Serial.write((uint8_t)0xFD);  // BT-HID: Start Byte
+  // Serial.write((uint8_t)0x09); // BT-HID: Data Length
+  Serial.write((uint8_t)len + 1);  // BT-HID: Data Length
+  Serial.write((uint8_t)0x01);     // BT-HID: Descriptor (1=Keyboard, 2=Mouse,
+                                   // 3=KybdMouse combo)
+  // Keyboard Data consists of a single byte for modifiers (Ctrl, alt, etc)
+  // Followed by 0x00
+  // Followed by 6 key codes (any or all may be 0x00 for no key pressed)
+  HIDRelay::ParseHIDData(hid, ep, is_rpt_id, len, buf);
 };
 
 #ifdef _COMBO
-class MouseRptParser : public HIDRelay 
-{
-    void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+class MouseRptParser : public HIDRelay {
+ public:
+  MouseRptParser(USB *p) : HIDRelay(p){};
+  void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len,
+                    uint8_t *buf);
 };
 
-void MouseRptParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
-//    DEBUG_PRINTLN("Mouse");
-    Serial.write((uint8_t)0xFD); // BT-HID: Start Byte
-    Serial.write((uint8_t)0x05); // BT-HID: Data Length
-    // Serial.write((uint8_t)len+1); // BT-HID: Data Length
-    Serial.write((uint8_t)0x02); // BT-HID: Descriptor (1=Keyboard, 2=Mouse, 3=KybdMouse combo)
-    // Mouse Data consists of a single byte for pressed buttons
-    // Followed by 0x00
-    // Followed by 1 byte each for X then Y movement
-    // Lastly 1 byte for wheel(optional?) 
-    HIDRelay::Parse(hid,is_rpt_id,len,buf);
-    if(len < 4){
-//      DEBUG_PRINTLN("ZeroPad");
-      for(uint8_t i=1; len+i<5; i++)
-        Serial.write((uint8_t)0x00);
-    }
+void MouseRptParser::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id,
+                                  uint8_t len, uint8_t *buf) {
+  //    DEBUG_PRINTLN("Mouse");
+  Serial.write((uint8_t)0xFD);  // BT-HID: Start Byte
+  Serial.write((uint8_t)0x05);  // BT-HID: Data Length
+  // Serial.write((uint8_t)len+1); // BT-HID: Data Length
+  Serial.write((uint8_t)0x02);  // BT-HID: Descriptor (1=Keyboard, 2=Mouse,
+                                // 3=KybdMouse combo)
+  // Mouse Data consists of a single byte for pressed buttons
+  // Followed by 0x00
+  // Followed by 1 byte each for X then Y movement
+  // Lastly 1 byte for wheel(optional?)
+  HIDRelay::ParseHIDData(hid, ep, is_rpt_id, len, buf);
+  if (len < 4) {
+    //      DEBUG_PRINTLN("ZeroPad");
+    for (uint8_t i = 1; len + i < 5; i++) Serial.write((uint8_t)0x00);
+  }
 };
 #endif
 
-USB     Usb;
+USB Usb;
 #ifdef _COMBO
-USBHub     Hub(&Usb);
-HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE > HidComposite(&Usb);
-HIDBoot<USB_HID_PROTOCOL_MOUSE>    HidMouse(&Usb);
+// USBHub     Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE> HidComposite(&Usb);
+HIDBoot<USB_HID_PROTOCOL_MOUSE> HidMouse(&Usb);
 #endif
-HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD> HidKeyboard(&Usb);
 
 uint32_t next_time;
 
-KbdRptParser KbdPrs;
+KbdRptParser KbdPrs(&Usb);
 #ifdef _COMBO
-MouseRptParser MousePrs;
+MouseRptParser MousePrs(&Usb);
 #endif
 
-void setup()
-{
-    Serial.begin( 115200 );
+void setup() {
+  ADCSRA = 0;           // disable ADC by setting ADCSRA register to 0
+  power_adc_disable();  // disable the clock to the ADC module
+  Serial.begin(115200);
 
 #if !defined(__MIPSEL__)
-    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+  while (!Serial)
+    ;  // Wait for serial port to connect - used on Leonardo, Teensy and other
+       // boards with built-in USB CDC serial connection
 #endif
 
-    DEBUG_PRINTLN("Start");
+  DEBUG_PRINTLN("Start");
 
-    if (Usb.Init() == -1)
-        DEBUG_PRINTLN("OSC did not start.");
+  if (Usb.Init() == -1) DEBUG_PRINTLN("OSC did not start.");
 
-    delay( 200 );
+  delay(200);
 
-    next_time = millis() + 5000;
+  next_time = millis() + 5000;
 
-#ifdef _COMBO
-    HidComposite.SetReportParser(0, &KbdPrs);
-    HidComposite.SetReportParser(1, &MousePrs);
-    HidMouse.SetReportParser(0, &MousePrs);
-#endif
-    HidKeyboard.SetReportParser(0, &KbdPrs);
-
+  //#ifdef _COMBO
+  //    HidComposite.SetReportParser(0, &KbdPrs);
+  //  HidComposite.SetReportParser(1, &MousePrs);
+  // HidMouse.SetReportParser(0, &MousePrs);
+  //#endif
+  // HidKeyboard.SetReportParser(0, &KbdPrs);
 }
 
-void loop()
-{
-    Usb.Task();
-}
-
-
-
-
-
-
-
-
+void loop() { Usb.Task(); }
